2730c50b871e185c7838ca47bdd4ab38b187546d
Neel Amin <neelamin27@gmail.com>
2018-11-02 00:58:31 -0400

    Grand loops

--- ptr.c
+++ ptr.c
@@ -3,16 +3,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main() 
+int main()
 {
-  int *num = malloc(sizeof(int) * 3);
-  num[0] = 3;
-  num[1] = 2;
-  num[2] = 1;
+  int i;
+  int *num = malloc(sizeof(int) * 1000);
+  for(i=0; i<1000; i++)
+  {
+  num[i] = i;
+  }
 
-  printf("%p -> %d\n", num, *num );
-  printf("%p -> %d\n", num+1, *(num+1) );
-  printf("%p -> %d\n", num+2, *(num+2) );
+  //num[0] = 3;
+  //num[1] = 2;
+  //num[2] = 1;
+
+  //printf("%p -> %d\n", num, *num );
+  //printf("%p -> %d\n", num+1, *(num+1) );
+  //printf("%p -> %d\n", num+2, *(num+2) );
+
+  for(i=0; i<1000; i++)
+  {
+  printf("%p -> %d\n", num+i, *(num+i) );
+  }
 
   return 0;
 }
