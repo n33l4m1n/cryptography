2f10538f4909ddc1455c3d42578e8476d1d0d42e
Neelkumar Amin <neelkumar.amin@outlook.com>
2023-11-09 23:16:07 -0500

    gcc ptr.c -o a.out

--- ptr.c
+++ ptr.c
@@ -1,29 +1,32 @@
 // Pointers in the C language.
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-  int i;
-  int *num = malloc(sizeof(int) * 1000);
-  for(i=0; i<1000; i++)
-  {
-  num[i] = i;
-  }
+int main() {
+    int i;
+    
+    // Allocate memory for an array of 1000 integers and store a pointer to it in 'num'.
+    int *num = malloc(sizeof(int) * 1000);
+    
+    // Check if memory allocation was successful.
+    if (num == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
 
-  //num[0] = 3;
-  //num[1] = 2;
-  //num[2] = 1;
+    // Initialize the array with values from 0 to 999.
+    for (i = 0; i < 1000; i++) {
+        num[i] = i;
+    }
 
-  //printf("%p -> %d\n", num, *num );
-  //printf("%p -> %d\n", num+1, *(num+1) );
-  //printf("%p -> %d\n", num+2, *(num+2) );
+    // Print the addresses and values of the elements in the array.
+    for (i = 0; i < 1000; i++) {
+        printf("%p -> %d\n", (void *)(num + i), num[i]);
+    }
 
-  for(i=0; i<1000; i++)
-  {
-  printf("%p -> %d\n", num+i, *(num+i) );
-  }
+    // Free the allocated memory to prevent memory leaks.
+    free(num);
 
-  return 0;
+    return 0;
 }
